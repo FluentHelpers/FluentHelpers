e5666e0a2ed1adf380b5b49fdf32459ada042c8b
csw <cahsul.dev@gmail.com>
2021-07-04 20:49:06 +0700

    TrimAllStrings

--- FluentHelpers/Extensions/Trim.cs
+++ FluentHelpers/Extensions/Trim.cs
@@ -1,13 +1,54 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace FluentHelpers.Extensions
 {
-    class Trim
+    public static class TrimExtensions
     {
         // trim
         // trim left
+
+        // source : https://stackoverflow.com/questions/7726714/trim-all-string-properties
+        public static TSelf TrimAllStrings<TSelf>(this TSelf obj)
+        {
+            if (obj != null)
+            {
+                if (obj is IEnumerable)
+                {
+                    foreach (var listItem in obj as IEnumerable)
+                    {
+                        listItem.TrimAllStrings();
+                    }
+                }
+                else
+                {
+                    BindingFlags flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy;
+
+                    foreach (PropertyInfo p in obj.GetType().GetProperties(flags))
+                    {
+                        Type currentNodeType = p.PropertyType;
+                        if (currentNodeType == typeof(String))
+                        {
+                            string currentValue = (string)p.GetValue(obj, null);
+                            if (currentValue != null)
+                            {
+                                p.SetValue(obj, currentValue.Trim(), null);
+                            }
+                        }
+                        // see http://stackoverflow.com/questions/4444908/detecting-native-objects-with-reflection
+                        else if (currentNodeType != typeof(object) && Type.GetTypeCode(currentNodeType) == TypeCode.Object)
+                        {
+                            p.GetValue(obj, null).TrimAllStrings();
+                        }
+                    }
+                }
+            }
+
+            return obj;
+        }
 
     }
 }
