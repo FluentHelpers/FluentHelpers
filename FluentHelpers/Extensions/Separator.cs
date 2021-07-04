20518e0208e709ec6e633afef58df4e608e7fef5
csw <cahsul.dev@gmail.com>
2021-07-04 17:14:21 +0700

    StringSeparator

---
+++ FluentHelpers/Extensions/Separator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FluentHelpers
+{
+    public static class SeparatorExtensions
+    {
+        public static string Separator(this string value, char Separator, int addEvery = 0)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (i % addEvery == 0)
+                    sb.Append(Separator);
+                sb.Append(value[i]);
+            }
+            sb.Remove(0, 1);
+            return sb.ToString();
+
+        }
+
+    }
+}
